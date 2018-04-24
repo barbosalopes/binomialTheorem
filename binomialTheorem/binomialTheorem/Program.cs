6008a77e9ef0b2348e1dc7116dd04e2226789b00
barbosalopes <barbosalopesdeveloper@gmail.com>
2018-04-24 20:39:35 -0300

    FIrst Commit

---
+++ binomialTheorem/binomialTheorem/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace binomialTheorem
+{
+    class Program
+    {
+        // Criar funções para calcular o fatorioal, o somatorio, a unidade do somatorio e o coeficiente
+        static void Main(string[] args)
+        {
+        }
+    }
+}
